f95b1aefc262fc92c577cc9d54ea26578d5e3174
Ryan Stringham <stringham@gmail.com>
2013-09-30 22:05:43 -0600

    Updated color scheme to BYU's (http://web.byu.edu/styleguide/colors)

--- js/names.js
+++ js/names.js
@@ -6,7 +6,7 @@
  */
 
 //set the colors for each life, in HEX
-var colors = ["", "#77cc33","#72C4C0","#E37341","#FFA500","#FFFF00","#222222"];
+var colors = ["#D1E4F6", "#ABC8E4","#628CB6","#003366","#001948","#001948","#001948","#001948"];
 
 /*
  * If you want to have the names show up on the page without entering them into
