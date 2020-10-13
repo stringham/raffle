7b3f501d2e15d56f3e8b73e86fec2df61b47516d
Chloe Hale <chloe@lucidchart.com>
2020-10-13 17:27:03 -0600

    Add ability to choose the number of winners per round.

--- js/raffle.js
+++ js/raffle.js
@@ -32,12 +32,22 @@ function getNames() {
 	});
 }
 
+var numberOfWinnersPerRound = 1;
+
 function process(){
-	var names = $('.name-text-field').val().split('\n');
+	var names = getNames();
 	imported = [];
-	map(getNames(), function(name){
+	map(names, function(name){
 		imported.push({'name':name});
-	});
+    });
+    
+    var numFromInput = parseInt($('#number-of-winners-input').val(), 10);
+    if (isNaN(numFromInput) || numFromInput < 1) {
+        numberOfWinnersPerRound = 1;
+    } else {
+        numberOfWinnersPerRound = Math.min(numFromInput, names.length);
+    }
+
 	$('.enter-names').hide(500, function(){
 		makeTicketsWithPoints();
 	});
@@ -117,7 +127,7 @@ function Ticket(name, points){
 			})
 			setTimeout(function() {
 				callback();
-			}, length == 2 ? 1000 : 3000/length);
+			}, length == 2 ? 1000 : 3000/(length - numberOfWinnersPerRound));
 		}
 	}
 }
@@ -172,7 +182,14 @@ var makeTicketsWithPoints = function(){
 			t.dom.appendTo($('body'));
 			tickets.push(t);
 		}
-	});
+    });
+
+    $('#participant-number').css('width', '').text(tickets.length);
+    
+    if (tickets.length < 1) {
+        return;
+    }
+
 	tickets.reverse();
 	size = 40;
 	$('.ticket').css('font-size', size + 'px');
@@ -181,7 +198,6 @@ var makeTicketsWithPoints = function(){
 		$('.ticket').css('font-size', size + 'px');
 	}
 
-	$('#participant-number').css('width', '').text(tickets.length);
 	setTimeout(function() {
 		map(tickets, function(ticket){
 			ticket.fixPosition();
@@ -218,35 +234,50 @@ var pickName = function(){
 	$('body').unbind('click');
 	
 	var choices = getChoices();
-	if(remainingParticipants() > 1){
+	if(remainingParticipants() > numberOfWinnersPerRound){
 		var remove = randomInt(choices.length);
 		choices[remove].decrement(choices.length, function(){
 			pickName();
 		});
 	} else {
 		if (removeWinners) {
-			var winner = choices[0].name;
-			for (var entry of imported) {
-				if (entry.name == winner) {
-					entry.points--;
-				}
-			}
-		}
-		choices = $(choices[0].dom);
-		var top = choices.css('top');
-		var left = choices.css('left');
-		var fontsize = choices.css('font-size');
-		var width = choices.width();
-		choices.click(function(){
-			inProgress = false;
-			choices.animate({'font-size':fontsize,'top':top,'left':left},'slow');
-			$('.ticket').show(500).unbind('click');
-			setTimeout(function(){
-				makeTicketsWithPoints();
-			}, 700);
-		});
-		choices.animate({'font-size':3*size +'px','top':(window.innerHeight/5) + 'px','left':(window.innerWidth/2 - width) + 'px'},1500, function(){
-			choices.animate({'left':(window.innerWidth/2 - choices.width()/2) + 'px'}, 'slow');
-		});
+            choices.forEach(choice => {
+                var winner = choice.name;
+                for (var entry of imported) {
+                    if (entry.name == winner) {
+                        entry.points--;
+                    }
+                }
+            });
+        }
+        
+        if (numberOfWinnersPerRound == 1) {
+            choices = $(choices[0].dom);
+            var top = choices.css('top');
+            var left = choices.css('left');
+            var fontsize = choices.css('font-size');
+            var width = choices.width();
+            choices.click(function(){
+                inProgress = false;
+                choices.animate({'font-size':fontsize,'top':top,'left':left},'slow');
+                $('.ticket').show(500).unbind('click');
+                setTimeout(function(){
+                    makeTicketsWithPoints();
+                }, 700);
+            });
+            choices.animate({'font-size':3*size +'px','top':(window.innerHeight/5) + 'px','left':(window.innerWidth/2 - width) + 'px'},1500, function(){
+                choices.animate({'left':(window.innerWidth/2 - choices.width()/2) + 'px'}, 'slow');
+            });
+        } else {
+            choices.forEach(c => {
+                $(c.dom).animate({
+                    'font-size':1.25*size,
+                });
+                $(c.dom).css('background', '#EE8800');
+            });
+            $('body').click(() => {
+                makeTicketsWithPoints();
+            });
+        }
 	}
 }
